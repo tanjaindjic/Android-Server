057b21473931419730e04b7137c0d05ddbf495e3
tanjaindjic <mali.patuljko@gmail.com>
2019-05-23 15:14:01 +0200

    init

---
+++ src/main/java/com/subarata/springbootandrest/SpringBootApp.java
@@ -0,0 +1,16 @@
+package com.subarata.springbootandrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Initiate spring boot
+@SpringBootApplication
+public class SpringBootApp {
+
+	public static void main(String[] args) {
+
+		// Set runtime class
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+
+}
