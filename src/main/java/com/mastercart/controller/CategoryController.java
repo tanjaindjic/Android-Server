fa94150174f0cd407030fdb226f54f739bc7689c
NinaMNina <ninamiladinovic@hotmail.com>
2019-05-26 15:17:17 +0200

    reseno dodavanje kategorije

--- src/main/java/com/mastercart/controller/CategoryController.java
+++ src/main/java/com/mastercart/controller/CategoryController.java
@@ -1,20 +1,20 @@
 package com.mastercart.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mastercart.model.Category;
-import com.mastercart.model.Product;
 import com.mastercart.model.User;
+import com.mastercart.model.enums.Role;
 import com.mastercart.security.TokenUtils;
 import com.mastercart.service.CategorySevice;
 import com.mastercart.service.UserService;
@@ -27,12 +27,18 @@ public class CategoryController {
 
 	@Autowired
 	private TokenUtils tokenUtils;
+	@Autowired
+	private UserService userService;
     
-    @PostMapping(value = "category", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Category> addCategory(@RequestHeader(value="Authorization") String Authorization, @RequestParam("name") String name){
+	@RequestMapping(value = "category", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Category> addCategory(@RequestHeader(value="Authorization") String Authorization, @RequestBody String name){
     	String email = tokenUtils.getUsernameFromToken(Authorization);
 	    if(email==null || email.isEmpty()) {
 	    	System.out.println("Pokusaj neautorizovanog dodavanja kategorije");	    	
+	    	return new ResponseEntity<>(null, HttpStatus.OK);
+	    }
+	    User user = userService.getUserByEmail(email);	    
+	    if(user == null || !user.getRole().equals(Role.ADMIN)) {
 	    	return new ResponseEntity<>(null, HttpStatus.OK);
 	    }
     	System.out.println("dodavanje kategorije");
